e7084dc1a2edf06a7bb155dc8b3f8d3a2913f977
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-11 00:20:06 -0700

    commit tree e7a26657811fda3641315e5bd92d182a5c9bcefe

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array) {
+    const result = array.find((element) => element.result === "W");
+    if (result) {
+        return result.year;
+        
+    } else {
+        return undefined;
+
+    }
+}
